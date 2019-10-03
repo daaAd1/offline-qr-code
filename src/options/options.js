c5e0af6b85043f800d2cbf829da5f2a996645370
Samuel Sepesi <15359784+daaAd1@users.noreply.github.com>
2019-10-03 15:46:15 +0200

    Add slovak translation

--- src/options/options.js
+++ src/options/options.js
@@ -5,6 +5,7 @@
  * @requires modules/OptionHandler
  */
 
+import { tips } from "/common/modules/data/Tips.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
@@ -15,7 +16,7 @@ import * as CustomOptionTriggers from "./modules/CustomOptionTriggers.js";
 CustomOptionTriggers.registerTrigger();
 AutomaticSettings.setDefaultOptionProvider(AddonSettings.getDefaultValue);
 AutomaticSettings.init();
-RandomTips.init().then(() => {
+RandomTips.init(tips).then(() => {
     RandomTips.setContext("options");
     RandomTips.showRandomTipIfWanted();
 });

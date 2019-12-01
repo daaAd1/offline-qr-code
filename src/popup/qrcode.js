15c553b29dd10ac03f571d32e33b1322e7afdc5d
Samuel Sepesi <15359784+daaAd1@users.noreply.github.com>
2019-12-01 15:49:29 +0100

    Add performance change

--- src/popup/qrcode.js
+++ src/popup/qrcode.js
@@ -6,12 +6,25 @@
  * @requires modules/InitQrCode
  */
 "use strict";
-
+import { tips } from "/common/modules/data/Tips.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 
 import "./modules/InitQrCode.js";
 
-RandomTips.init().then(() => {
-    RandomTips.setContext("popup");
-    RandomTips.showRandomTipIfWanted();
-});
+const t0 = performance.now();
+
+if (Math.random() < 0.2) {
+    RandomTips.init(tips).then(() => {
+        RandomTips.setContext("popup");
+        const t2 = performance.now();
+        console.log(`Random init when tip shows took ${t2 - t0} milliseconds.`);
+        RandomTips.showRandomTip();
+    });
+} else {
+    const t2 = performance.now();
+    console.log(`Random init WHOLE took ${t2 - t0} milliseconds.`);
+    console.info("show no random tip, because randomize did not pass");
+    throw Promise.reject(
+        new Error("show no random tip, because randomize did not pass")
+    );
+}
